0723533113a5837a977610439d73c4785ee23fd7
gzavaglia <gio.zavaglia@gmail.com>
2019-10-19 11:56:09 -0500

    added routines to users and create routines

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -10,8 +10,25 @@ class RoutinesController < ApplicationController
 
     def new
         @user = User.find(params[:user_id])
-        @routine = Routine.new
+        @routine = @user.routines.new
+        
     end
 
+    def create
+        @user = User.find(params[:user_id])
+        @routine = @user.routines.new(routine_params)
+        @routine.user = User.find_by(id: params[:user_id])
+        if @routine.save
+            redirect_to user_routines_path(@user)
+        else
+          render :new
+        end
+    end
+
+      private
+
+  def routine_params
+    params.require(:routine).permit(:title, :date, :workout_id)
+  end
 
 end
--- app/models/routine.rb
+++ app/models/routine.rb
@@ -1,4 +1,12 @@
 class Routine < ApplicationRecord
     belongs_to :user
     belongs_to :workout
+    def workout_name=(workout)
+        self.workout = Workout.find_or_create_by(name: workout)
+    end
+    
+    def workout_name
+        self.workout.name if self.workout
+    end
+    
 end
